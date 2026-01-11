2d7ad32346bf8ee17ddfb8ff81bc9823ad98d0a2
Anyang Ateny <anyangateny@outlook.com>
2026-01-12 04:28:27 +1030

    feat: implemented a reserve bench mark for vector

--- benchmarks/vector_bench.cpp
+++ benchmarks/vector_bench.cpp
@@ -18,6 +18,10 @@ namespace detect {
 template <class T, class E>
 concept has_push_back = requires(T t, E e) { t.push_back(e); };
 
+template <class T, class E>
+concept has_reserve = requires(T t, E e) { t.reserve(e); };
+
+
 } // namespace detect
 
 // Benchmark
@@ -51,10 +55,44 @@ void benchmark_push_back() {
     });
 }
 
+void benchmark_reserve() {
+    // tests a simple reserve
+    constexpr size_t N = 1'000'000;
+
+
+    if constexpr (!detect::has_reserve<MyVector<int>, size_t>) {
+        std::cout << "[SKIP] MyVector missing reserve()";
+        return;
+    }
+
+    ankerl::nanobench::Bench bench;
+    bench.title("reserve 1M capacity").unit("element").batch(N);
+
+    bench.run("std::vector", [&] {
+        std::vector<int> v;
+        for (size_t i = 1; i <= N; i++) {
+            v.reserve(i);
+            v.shrink_to_fit();
+        }
+        ankerl::nanobench::doNotOptimizeAway(v);
+    });
+
+    bench.run("MyVector", [&] {
+        MyVector<int> v;
+        for (size_t i = 1; i <= N; i++) {
+            v.reserve(i);
+            v.shrink_to_fit();
+        }
+        ankerl::nanobench::doNotOptimizeAway(v);
+    });
+}
+
 int main() {
     std::cout << "=== MyVector Benchmark ===\n\n";
 
     benchmark_push_back();
+
+    benchmark_reserve();
 
     std::cout << "\nDone!\n";
     return 0;
